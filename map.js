aaef4866558803e40aac511e6b5fd152a5402494
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2024-12-25 20:31:41 +0530

    extractNames and extractAges implemented

--- map.js
+++ map.js
@@ -238,16 +238,24 @@ const sortedLettersOf = function (strings) {
 };
 
 // wrap strings in brackets ["apple", "banana"] => ["[apple]", "[banana]"]
-const wrappedStringsOf = function (strings) {};
+// const wrappedStringsOf = function (strings) {
+//   return strings.map((string) => [string]);
+// };
 
 // extract names from [{ name: "Alice" }, { name: "Bob" }] => ["Alice", "Bob"]
-const extractNames = function (objects) {};
+const extractNames = function (objects) {
+  return objects.map((object) => object.name);
+};
 
 // extract ages from [{ age: 25 }, { age: 30 }] => [25, 30]
-const extractAges = function (objects) {};
+const extractAges = function (objects) {
+  return objects.map((object) => object.age);
+};
 
 // extract the first letters of names from [{ name: "Alice" }, { name: "Bob" }] => ["A", "B"]
-const firstLettersOfNames = function (objects) {};
+const firstLettersOfNames = function (objects) {
+
+};
 
 // calculate areas from [{ width: 2, height: 3 }, { width: 4, height: 5 }] => [6, 20]
 const calculateAreas = function (rectangles) {};
